6d9a4ac2145e786737411ff88c9bada9234a3966
noHost01 <ljh12081308@naver.com>
2022-08-05 12:27:09 +0900

    세팅

---
+++ src/test/java/com/chatweb/chatsite/ChatsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatweb.chatsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
